b11668dbb687934fa57a26d2cc52c17f5f6af899
Poornachandra_Shetty <poornachandra14046@gmail.com>
2025-09-13 15:34:59 +0530

    Added Html,css and Js

--- studenthub.py
+++ studenthub.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'studenthub'
+    return render_template('studenthub.html')
+
 if __name__=="__main__":
     app.run(debug=True)
